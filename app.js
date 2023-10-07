e64ede82f9a3182ba4200bc045514bcfc45094a3
LeandroBoxler <leandroboxler@gmail.com>
2023-10-07 14:19:16 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ("express")
+const app = express()
+const path = require("path")
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/about",(req,res)=>res.sendFile(path.join(__dirname,"views","about.html")))
+app.get("/music",(req,res)=>res.sendFile(path.join(__dirname,"views","music.html")))
+app.get("/contact",(req,res)=>res.sendFile(path.join(__dirname,"views","contact.html")))
+
+
+app.listen(3030,()=>console.log("vamo los pibes"))
